344e133ae7c06002c5c01d2cd0f2b6cec9453ede
Kevin Nguyen <hngu0806@uni.sydney.edu.au>
2025-05-06 12:30:22 +1000

    Back to sign up option

--- api/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS todos (
-    id SERIAL PRIMARY KEY,
-    task TEXT NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    completed BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (user_email) REFERENCES users(email)
-); 
